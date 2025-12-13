34fb2bb97d505d9b83602ea5bf0cd695cb2dcf0b
Dauren Ibrayev <daurenibraev1995@gmail.com>
2025-12-13 19:20:43 +0500

    fixed issues, redid task

--- js/homework-8.js
+++ js/homework-8.js
@@ -6,7 +6,7 @@ const cardListTemplate = document.querySelector('.card-list__template');
 //task 4
 
 const headingsString = cardsContent.reduce((acc, item) => {
-  return acc ? acc + "; " + item.heading : item.heading;
+  return acc ? acc + "; " + item.name : item.name;
 }, "");
 
 console.log(headingsString);
@@ -15,7 +15,7 @@ console.log(headingsString);
 
 const descriptionMap = cardsContent.reduce((acc, item) => {
   acc.push({
-    [item.heading]: item.description
+    [item.name]: item.description
   });
   return acc;
 }, []);
@@ -28,36 +28,28 @@ function showCards() {
   let cardsCount = Number(prompt('Сколько карточек отобразить?'))
 
   if (cardsCount > 0 && cardsCount <= 5) {
-    cardList.innerHTML = "";
     cardsContent.slice(0, cardsCount).forEach(card => {
       const cardClone = cardListTemplate.content.cloneNode(true);
 
-      //Картинки
-      const imgElement = cardClone.querySelector('.card__image')
-      imgElement.src = card.img;
-      imgElement.alt = card.alt || card.heading;
+      cardClone.querySelector('.card__image').src = `images/${card.img}.png`
+      cardClone.querySelector('.card__heading').textContent = card.name;
+      cardClone.querySelector('.card__text').textContent = card.description;
 
-      cardClone.querySelector('.card__descr').textContent = card.description;
-      cardClone.querySelector('.card__heading').textContent = card.heading;
-      cardClone.querySelector('.card__text').textContent = card.text;
-      cardClone.querySelector('.card__list-title').textContent = card.listTitle;
 
-      //Элементы списка
-      const cardInnerList = cardClone.querySelector('.card__list')
-      cardInnerList.innerHTML = "";
-      card.listRow.forEach(item => {
+      const cardInnerList = cardClone.querySelector('.card__composition')
+      card.composition.forEach(item => {
         const liElement = document.createElement('li');
         liElement.textContent = item;
         cardInnerList.appendChild(liElement)
       })
 
-
-      cardClone.querySelector('.card__price-title').textContent = card.priceTitle;
       cardClone.querySelector('.card__price').textContent = card.price;
       cardList.appendChild(cardClone)
       })
   } else {
       alert('Введите корректное значение')
+      cardsCount = Number(prompt('Сколько карточек отобразить?'))
+      showCards()
     }
 }
 
